1153068e4001b48489c9015de1a7c56039bd897f
mohamedeltair <moodatayr@hotmail.com>
2018-12-03 23:12:55 +0200

    Preprocessed the calculation of cells and blocks of an image to avoid recalculating them over and over again

--- CarDetection/CarDetection_HOG.py
+++ CarDetection/CarDetection_HOG.py
@@ -11,6 +11,12 @@ from skimage import data , io , filters #Data for images , io for imshow , filte
 import matplotlib.pyplot as plt
 import colorsys
 get_ipython().run_line_magic('matplotlib', 'inline')
+cell_size = 8
+n_angles = 18
+angle_step = 20
+hist_grid = None
+block_grid = None
+win_size = 64
 #from CarDetection_SVM.py import p
 
 
@@ -43,15 +49,19 @@ def get_mags_and_dirs(img):
 # In[4]:
 
 
-def get_hist_grid(sub_img, cell_size):
+def get_hist_grid(sub_img):
     ri, ci = sub_img.shape[0], sub_img.shape[1] 
-    r, c = (ri-1)//cell_size+1, (ci-1)//cell_size+1
-    hist_grid = np.zeros((r, c, 18), dtype=float)
-    for i in range(r):
-        for j in range(c):
+    #r, c = (ri-1)//cell_size+1, (ci-1)//cell_size+1
+    r, c = ri-cell_size+1, ci-cell_size+1
+    global hist_grid
+    hist_grid = np.zeros((r, c, n_angles), dtype=float)
+    for i in range(0, r, cell_size):
+        for j in range(0, c, cell_size):
             cur_cell = np.zeros((cell_size, cell_size, 3), sub_img.dtype)
-            ilo, iup, jlo, jup = i*cell_size, min((i+1)*cell_size, ri), j*cell_size, min((j+1)*cell_size, ci)
-            cur_cell[:min(cell_size, iup-ilo),:min(cell_size, jup-jlo),:] = sub_img[ilo:iup,jlo:jup,:]
+            #ilo, iup, jlo, jup = i*cell_size, min((i+1)*cell_size, ri), j*cell_size, min((j+1)*cell_size, ci)
+            #cur_cell[:min(cell_size, iup-ilo),:min(cell_size, jup-jlo),:] = sub_img[ilo:iup,jlo:jup,:]
+            ilo, iup, jlo, jup = i, i+cell_size, j, j+cell_size
+            cur_cell[:cell_size,:cell_size,:] = sub_img[ilo:iup,jlo:jup,:]
             mags, dirs = get_mags_and_dirs(cur_cell)
             for k in range(cell_size):
                 for l in range(cell_size):
@@ -61,25 +71,25 @@ def get_hist_grid(sub_img, cell_size):
                             max_mag, its_dir = mags[k, l, m], dirs[k, l, m]
                     if(its_dir < 0):
                         its_dir = 360 + its_dir
-                    flr = int(np.floor(its_dir/20))
-                    lo, up = flr*20, (flr+1)*20
+                    flr = int(np.floor(its_dir/angle_step))
+                    lo, up = flr*angle_step, (flr+1)*angle_step
                     dl, du = its_dir-lo, up-its_dir
-                    hist_grid[i, j, (lo//20)%18] += (1-dl/20.0)*max_mag
-                    hist_grid[i, j, (up//20)%18] += (1-du/20.0)*max_mag
-    return hist_grid
+                    hist_grid[i, j, (lo//angle_step)%n_angles] += (1-dl/float(angle_step))*max_mag
+                    hist_grid[i, j, (up//angle_step)%n_angles] += (1-du/float(angle_step))*max_mag
+    #return hist_grid
 
 
 # In[5]:
 
-
-def Block_Normalization(h_array):
-    R,C = len(h_array),len(h_array[0])
-    giant_hist = []
-    
-    for i in range(R-1):
-        for j in range(C-1):
+def Block_Normalization_Preprocess():
+    hist_grid_r, hist_grid_c = hist_grid.shape[0], hist_grid.shape[1]
+    R, C = hist_grid_r-cell_size, hist_grid_c-cell_size
+    global block_grid
+    block_grid = np.zeros((R, C, n_angles*4), dtype=float)
+    for i in range(0, R, cell_size):
+        for j in range(0, C, cell_size):
             #Row-major concatenation
-            big_hist = list(h_array[i][j]) + list(h_array[i][j+1]) + list(h_array[i+1][j]) + list(h_array[i+1][j+1])
+            big_hist = list(hist_grid[i,j]) + list(hist_grid[i,j+cell_size]) + list(hist_grid[i+cell_size,j]) + list(hist_grid[i+cell_size,j+cell_size])
             
             #Squaring the values
             big_hist_sq = [h ** 2 for h in big_hist]
@@ -93,8 +103,17 @@ def Block_Normalization(h_array):
             #Normalizing the historgram
             big_hist_norm = [0.0 if big_hist_mag == 0 else m / big_hist_mag for m in big_hist]
             
+            block_grid[i,j] = big_hist_norm
+
+def Block_Normalization(si,sj):
+    R,C = win_size, win_size
+    giant_hist = []
+    
+    for i in range(si, si+R-2*cell_size+1, cell_size):
+        for j in range(sj, sj+C-2*cell_size+1, cell_size):
+            
             #Concatinating all the histograms
-            giant_hist += big_hist_norm
+            giant_hist += list(block_grid[i,j])
             
     return giant_hist
 
@@ -111,4 +130,3 @@ hist_grid = get_hist_grid(img, 8)
 giant_hist = Block_Normalization(hist_grid)
 print("length of giant histogram: ", len(giant_hist))
 '''
-
