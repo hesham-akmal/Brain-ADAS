66788ece12540b69de1fa9668905760898eeac60
mohamedeltair <moodatayr@hotmail.com>
2018-12-14 06:09:18 +0200

    Performance enhancements
    1) Optimized gradient calculations by pre-processing it
    2) Used LUV instead of RBG
    3) Used HOG for every channel which increased the accuracy to about 99.75
    4) Used multiple down-scaled version of image in sliding window to detect big cars

--- CarDetection/CarDetection_HOG.py
+++ CarDetection/CarDetection_HOG.py
@@ -8,15 +8,26 @@ import math
 import pandas as pd
 import numpy as np
 from skimage import data , io , filters #Data for images , io for imshow , filters for prewitt
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import colorsys
+import cv2
 get_ipython().run_line_magic('matplotlib', 'inline')
 cell_size = 8
+n_block = 2
+win_size = 64
+hists_per_dim =(win_size-cell_size*n_block)//cell_size+1
 n_angles = 18
 angle_step = 20
 hist_grid = None
 block_grid = None
-win_size = 64
+grad_mag_grid = None
+grad_dir_grid = None
+max_angle = 360
+n_block_vals = n_block*n_block*n_angles
+n_win_blocks = hists_per_dim*hists_per_dim
+n_win_vals = n_win_blocks * n_block_vals
+
 #from CarDetection_SVM.py import p
 
 
@@ -31,6 +42,10 @@ def display(img, isgray):
         plt.imshow(img)
     plt.show()
 
+def readimg(name):
+    img = io.imread(name)
+    return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+    
 
 # In[3]:
 
@@ -48,48 +63,52 @@ def get_mags_and_dirs(img):
 
 # In[4]:
 
+def preprocess_grad_grids(img):
+    grad_mag_3d, grad_dir_3d = get_mags_and_dirs(img)
+    r, c = img.shape[0], img.shape[1]
+    global grad_mag_grid, grad_dir_grid
+    grad_mag_grid, grad_dir_grid = np.zeros((r, c), dtype=float), np.zeros((r, c), dtype=float)
+    for i in range(r):
+        for j in range(c):
+            max_mag, its_dir = 0.0, 0.0
+            for k in range(3):
+                if(grad_mag_3d[i,j,k] > max_mag):
+                    max_mag, its_dir = grad_mag_3d[i,j,k], grad_dir_3d[i,j,k]
+            if(its_dir < 0):
+                its_dir = max_angle + its_dir
+            grad_mag_grid[i,j], grad_dir_grid[i,j] = max_mag, its_dir
 
 def get_hist_grid(sub_img):
     ri, ci = sub_img.shape[0], sub_img.shape[1] 
     #r, c = (ri-1)//cell_size+1, (ci-1)//cell_size+1
-    r, c = ri-cell_size+1, ci-cell_size+1
+    r, c = ri//cell_size, ci//cell_size
     global hist_grid
     hist_grid = np.zeros((r, c, n_angles), dtype=float)
-    for i in range(0, r, cell_size):
-        for j in range(0, c, cell_size):
-            cur_cell = np.zeros((cell_size, cell_size, 3), sub_img.dtype)
-            #ilo, iup, jlo, jup = i*cell_size, min((i+1)*cell_size, ri), j*cell_size, min((j+1)*cell_size, ci)
-            #cur_cell[:min(cell_size, iup-ilo),:min(cell_size, jup-jlo),:] = sub_img[ilo:iup,jlo:jup,:]
-            ilo, iup, jlo, jup = i, i+cell_size, j, j+cell_size
-            cur_cell[:cell_size,:cell_size,:] = sub_img[ilo:iup,jlo:jup,:]
-            mags, dirs = get_mags_and_dirs(cur_cell)
+    preprocess_grad_grids(sub_img)
+    for i in range(0, ri-cell_size+1, cell_size):
+        for j in range(0, ci-cell_size+1, cell_size):
             for k in range(cell_size):
                 for l in range(cell_size):
-                    max_mag, its_dir = 0, 0
-                    for m in range(3):
-                        if(mags[k, l, m] > max_mag):
-                            max_mag, its_dir = mags[k, l, m], dirs[k, l, m]
-                    if(its_dir < 0):
-                        its_dir = 360 + its_dir
+                    max_mag, its_dir = grad_mag_grid[i+k,j+l], grad_dir_grid[i+k,j+l]
                     flr = int(np.floor(its_dir/angle_step))
                     lo, up = flr*angle_step, (flr+1)*angle_step
                     dl, du = its_dir-lo, up-its_dir
-                    hist_grid[i, j, (lo//angle_step)%n_angles] += (1-dl/float(angle_step))*max_mag
-                    hist_grid[i, j, (up//angle_step)%n_angles] += (1-du/float(angle_step))*max_mag
-    #return hist_grid
+                    hist_grid[i//cell_size, j//cell_size, (lo//angle_step)%n_angles] += (1-dl/float(angle_step))*max_mag
+                    hist_grid[i//cell_size, j//cell_size, (up//angle_step)%n_angles] += (1-du/float(angle_step))*max_mag
 
 
 # In[5]:
 
 def Block_Normalization_Preprocess():
     hist_grid_r, hist_grid_c = hist_grid.shape[0], hist_grid.shape[1]
-    R, C = hist_grid_r-cell_size, hist_grid_c-cell_size
+    R, C = hist_grid_r-n_block+1, hist_grid_c-n_block+1
     global block_grid
-    block_grid = np.zeros((R, C, n_angles*4), dtype=float)
-    for i in range(0, R, cell_size):
-        for j in range(0, C, cell_size):
+    block_grid = np.zeros((R, C, n_angles*n_block*n_block), dtype=float)
+    for i in range(0, R, 1):
+        for j in range(0, C, 1):
             #Row-major concatenation
-            big_hist = list(hist_grid[i,j]) + list(hist_grid[i,j+cell_size]) + list(hist_grid[i+cell_size,j]) + list(hist_grid[i+cell_size,j+cell_size])
+            #big_hist = list(hist_grid[i:i+n_block,j:j+n_block].reshape((n_block_vals)))
+            big_hist = hist_grid[i:i+n_block,j:j+n_block].reshape((n_block_vals))
             
             #Squaring the values
             big_hist_sq = [h ** 2 for h in big_hist]
@@ -109,11 +128,14 @@ def Block_Normalization(si,sj):
     R,C = win_size, win_size
     giant_hist = []
     
-    for i in range(si, si+R-2*cell_size+1, cell_size):
-        for j in range(sj, sj+C-2*cell_size+1, cell_size):
+    si = si//cell_size
+    sj = sj//cell_size
+    
+    #Concatinating all the histograms
+    #giant_hist = list(block_grid[si:si+hists_per_dim,sj:sj+hists_per_dim].reshape(n_win_vals))
+    giant_hist = block_grid[si:si+hists_per_dim,sj:sj+hists_per_dim].reshape(n_win_vals)
             
-            #Concatinating all the histograms
-            giant_hist += list(block_grid[i,j])
+            
             
     return giant_hist
 
