2bde241b06184b7917698eef3dcf8b80ffc31da2
FatemaMahmoud <fatma.mahmoud97@hotmail.com>
2019-05-27 04:55:36 +0200

    modified version of train test split fn

--- EEG Work/signals_classification.py
+++ EEG Work/signals_classification.py
@@ -59,7 +59,50 @@ def get_drop_cols(i1, electrodes_len, newCols):
         dropCols.append([name + '.' + str(num) for num in nums])
     return np.array(dropCols).flatten()
 
-def classify_diff_intervals_emotive(fnames, electrodes):
+def apply_filter(pos, neg):
+    fc = 0.02
+    b = 0.08
+    N = int(np.ceil((4 / b)))
+    if not N % 2: N += 1
+    n = np.arange(N)
+
+    sinc_func = np.sinc(2 * fc * (n - (N - 1) / 2.))
+    window = 0.42 - 0.5 * np.cos(2 * np.pi * n / (N - 1)) + 0.08 * np.cos(4 * np.pi * n / (N - 1))
+    sinc_func = sinc_func * window
+    sinc_func = sinc_func / np.sum(sinc_func)
+    
+    filter_pos = np.zeros(pos.shape)
+    filter_neg = np.zeros(neg.shape)
+    
+    for i in range(len(pos)):
+        for j in range(14):
+            s = list(pos.iloc[i][j*192:j*192+192])
+            filter_pos[i][j*192:j*192+192] = np.convolve(s, sinc_func)[:192]
+            
+    for i in range(len(neg)):
+        for j in range(14):
+            s = list(neg.iloc[i][j*192:j*192+192])
+            filter_neg[i][j*192:j*192+192] = np.convolve(s, sinc_func)[:192]
+            
+    filter_pos =  pd.DataFrame(data=filter_pos, columns=pos.columns)
+    filter_neg =  pd.DataFrame(data=filter_neg, columns=neg.columns) 
+            
+    return filter_pos, filter_neg
+
+def train_test_split_data(pos, neg, test_size):
+    pos_size_test = int(len(pos)*test_size)
+    neg_size_test = int(len(neg)*test_size)
+    all_test = pos[:pos_size_test].append(neg[:neg_size_test])
+    all_test.sample(frac=1)
+    y_test = all_test['y']
+    X_test = all_test.drop(['y'], axis = 1)
+    all_train = pos[pos_size_test:].append(neg[neg_size_test:])
+    all_train.sample(frac=1)
+    y_train = all_train['y']
+    X_train = all_train.drop(['y'], axis = 1)
+    return X_train, X_test, y_train, y_test
+
+def classify_diff_intervals(fnames, electrodes, filter_apply):
     LDAClassifier = LinearDiscriminantAnalysis()
     cols = make_columns_drawing(electrodes)
     newCols = make_columns_drawing(electrodes)
@@ -69,6 +112,8 @@ def classify_diff_intervals_emotive(fnames, electrodes):
         print(f)
         for i in np.arange(12, 180, 12):
             pos, neg = upload_pos_neg(f)
+            if(filter_apply):
+                pos, neg = apply_filter(pos, neg)
             pos, neg = select_electrodes(electrodes, pos, neg)
             dropCols = get_drop_cols(i, electrodes_len, newCols)
             #print(dropCols)
@@ -77,10 +122,11 @@ def classify_diff_intervals_emotive(fnames, electrodes):
             mean_features_pos, mean_features_neg = time_intervals_features(pos, neg, i, int(i*6/12), electrodes_len, cols)
             mean_features_pos['y'] = 1
             mean_features_neg['y'] = 0
-            all_data = mean_features_pos.append(mean_features_neg)
-            y = all_data['y']
-            X = all_data.drop(['y'], axis=1)
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1000)
+            #all_data = mean_features_pos.append(mean_features_neg)
+            #y = all_data['y']
+            #X = all_data.drop(['y'], axis=1)
+            #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1000)
+            X_train, X_test, y_train, y_test = train_test_split_data(mean_features_pos, mean_features_neg, test_size=0.3)
             X_train = np.nan_to_num(X_train)
             X_test = np.nan_to_num(X_test)
             #X_train = preprocessing.scale(X_train)
